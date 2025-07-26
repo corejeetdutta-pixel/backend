35dafc8ff6497205fc6818e64962a26f603e129c
JeetDutta-Bursana <bursana.jeet@gmail.com>
2025-07-26 23:26:19 +0530

    Initial commit with backend code

--- src/main/java/com/recruitment/controller/EmployeeController.java
+++ src/main/java/com/recruitment/controller/EmployeeController.java
@@ -7,6 +7,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import com.recruitment.entity.Employee;
 import com.recruitment.repository.EmployeeRepo;
 
