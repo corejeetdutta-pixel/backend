82afe93b48c7917e699c557c7170273d44fbf788
JeetDutta-Bursana <bursana.jeet@gmail.com>
2025-07-26 19:22:28 +0530

    manage table name

--- recruitment_backend-main/README.md
+++
@@ -1,1 +0,0 @@
-# recruitmente2backendpart
--- recruitment_backend-main/src/main/resources/db/migration/V2__add_agreed_to_terms_to_employees.sql
+++
@@ -1,2 +0,0 @@
--- Migration: Add agreed_to_terms column to employees table
-ALTER TABLE employees ADD COLUMN IF NOT EXISTS agreed_to_terms BOOLEAN NOT NULL DEFAULT FALSE;
