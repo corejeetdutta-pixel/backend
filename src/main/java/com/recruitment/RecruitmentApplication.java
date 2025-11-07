3d2560fadbd7455ada211d3aec6f02588f543e14
corejeetdutta-pixel <your-email-linked-to-that-account>
2025-11-07 23:20:17 +0530

    database bug fix

--- src/main/java/com/recruitment/RecruitmentApplication.java
+++ src/main/java/com/recruitment/RecruitmentApplication.java
@@ -1,11 +1,14 @@
 package com.recruitment;
 
+import org.flywaydb.core.Flyway;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.autoconfigure.flyway.FlywayAutoConfiguration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-
-@SpringBootApplication(scanBasePackages = "com.recruitment")
+@SpringBootApplication(scanBasePackages = "com.recruitment",exclude = { FlywayAutoConfiguration.class })
 @EntityScan(basePackages = "com.recruitment.entity")
 @EnableJpaRepositories(basePackages = "com.recruitment.repository")
 public class RecruitmentApplication {
--- src/main/resources/db/migration/V4__rename_user_table_to_users.sql
+++
@@ -1,2 +0,0 @@
--- Rename the user table to users for PostgreSQL reserved keyword fix
-ALTER TABLE IF EXISTS "user" RENAME TO users;
